d5464b82e87e4cad8bb27d054231db7369670416
Swarnim Arun <swarnimarun11@gmail.com>
2026-02-07 20:14:18 +0530

    fix: portability

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,25 +1,42 @@
 if status is-interactive
-    source (/Users/swarnimarun/.cargo/bin/starship init fish --print-full-init | psub)
-    fish_add_path /opt/homebrew/bin
-    fish_add_path $HOME/.cargo/bin
-
-    set -gx PNPM_HOME /Users/swarnimarun/Library/pnpm
-    set -gx EDITOR hx
-    set -gx GPG_TTY (tty)
-    set -gx DOTNET_ROOT /opt/homebrew/opt/dotnet/libexec
-    fish_add_path $PNPM_HOME
-    alias nvim hx
-
-    # The next line updates PATH for the Google Cloud SDK.
-    bass source '/Users/swarnimarun/gcloud/google-cloud-sdk/path.bash.inc'
-
-    # The next line enables shell command completion for gcloud.
-    bass source '/Users/swarnimarun/gcloud/google-cloud-sdk/completion.bash.inc'
-
-    # bun
-    set -gx BUN_INSTALL "$HOME/.bun"
-    set -gx PATH "$BUN_INSTALL/bin:$PATH"
+    source (starship init fish --print-full-init | psub)
 end
 
-# pull in the secrets
-source ~/.config/fish/.secrets.fish
+# setup cargo
+fish_add_path $HOME/.cargo/bin
+
+if test -d /opt/homebrew
+    fish_add_path /opt/homebrew/bin
+    set -gx DOTNET_ROOT /opt/homebrew/opt/dotnet/libexec
+end
+
+set -gx PNPM_HOME $HOME/Library/pnpm
+set -gx GPG_TTY (tty)
+fish_add_path $PNPM_HOME
+
+if test -f /usr/lib/helix/hx
+    set -gx EDITOR helix
+    alias nvim helix
+    alias hx helix
+else if command -q hx
+    set -gx EDITOR hx
+    alias nvim hx
+    alias helix hx
+end
+
+if test -d "$HOME/gcloud"
+    # The next line updates PATH for the Google Cloud SDK.
+    bass source "$HOME/gcloud/google-cloud-sdk/path.bash.inc"
+
+    # The next line enables shell command completion for gcloud.
+    bass source "$HOME/gcloud/google-cloud-sdk/completion.bash.inc"
+end
+
+# bun
+set -gx BUN_INSTALL "$HOME/.bun"
+set -gx PATH "$BUN_INSTALL/bin:$PATH"
+
+# pull in the secrets; if they exist
+if test -f ~/.config/fish/.secrets.fish
+    source ~/.config/fish/.secrets.fish
+end
